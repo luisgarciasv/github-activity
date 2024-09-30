a238ed54ec7edf35a99effe73154030e98063a0d
luisgarciasv <luisgarciasv3@gmail.com>
2024-09-30 15:10:38 -0600

    feat: error handling for missing argument

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import { httpReq } from "./lib/promisify.js"
 
-httpReq('luisgarciasv')
-    .then( res => console.log(res))
-    .catch( err => console.log(err))
+const [username] = process.argv.slice(2)
+
+if(!username){
+    console.warn(`
+username not defined. usage: github-activity <username>
+exaemple: github-activity luisgarciasv 
+        `)
+} else {
+    httpReq(username)
+        .then( res => console.log(res))
+        .catch( err => console.log(err))
+}
